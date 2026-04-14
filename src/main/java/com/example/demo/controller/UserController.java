c40dabf7d6f72ca53594e7291f0c9770785bdfbc
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-04-14 13:59:29 +0700

    Menambahkan fitur proses login dengan validasi username dan password

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UseContainer {
@@ -11,4 +14,17 @@ public class UseContainer {
     @GetMapping("/")
     public String loginpage() {
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String username,
+                        @RequestParam String password,
+                        Model model) {
+        if (USERNAME.equals(username) && PASSWORD.equals(password)) {
+            return "home";
+        } else {
+            model.addAttribute("error",
+                    "Invalid username or password");
+            return "login";
+        }
     }
