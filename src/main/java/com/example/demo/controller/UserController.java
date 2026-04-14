f2cdf79cc34a0b584515d943af48cd18e1dd9b5a
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-04-14 13:54:57 +0700

    Menambahkan controller UseContainer dengan autentikasi sederhana

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,4 +1,8 @@
 package com.example.demo.controller;
 
-public class UserController {
-}
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UseContainer {
+    private final String USERNAME = "admin";
+    private final String PASSWORD = "20230140039";
