a591a77240888da4e935df9a0c1af18184a4f2db
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-04-14 13:58:12 +0700

    Menambahkan endpoint untuk menampilkan halaman login

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UseContainer {
     private final String USERNAME = "admin";
     private final String PASSWORD = "20230140039";
+
+    @GetMapping("/")
+    public String loginpage() {
+        return "login";
+    }
